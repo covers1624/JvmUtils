98ef59ea336a6959155b6d88c70143d627092a5f
covers1624 <covers1624@gmail.com>
2025-02-21 20:35:53 +1030

    Start the real cli implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
+use clap::{Args, Parser, Subcommand};
 use env_logger::Env;
 use jvm_utils::locator::LocatorBuilder;
+use std::io;
 
-fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    /// Enable verbose output
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Lists all jvms on the system.
+    List(ListArgs)
+}
+
+#[derive(Args)]
+struct ListArgs {}
+
+fn main() -> io::Result<()> {
+    let cli = Cli::parse();
+    env_logger::Builder::from_env(Env::default().default_filter_or(if cli.verbose { "debug" } else { "info" })).init();
+
+    match cli.command {
+        Commands::List(_) => list()
+    }
+}
+
+fn list() -> io::Result<()> {
     let locator = LocatorBuilder::new()
         .with_platform_locator()
         .with_intellij_locator()
